8aa630f4b42d816b2732a763d8c10b4211797988
Tomek Szpinda <tszpinda@gmail.com>
2021-10-16 05:58:04 +0100

    Add message tail logic

--- app/src/main/java/tszpinda/chat/Data.kt
+++ app/src/main/java/tszpinda/chat/Data.kt
@@ -2,6 +2,33 @@ package tszpinda.chat
 enum class MessageType {
     IN, OUT
 }
-class Message (val text: String, val type: MessageType, val time: Long = System.currentTimeMillis())
+class MessageRaw (val text: String, val type: MessageType, val time: Long = System.currentTimeMillis())
+class Message (val text: String, val type: MessageType, val time: Long = System.currentTimeMillis(), val tail: Boolean = false)
 
 class Messages (val data: MutableList<Message>)
+
+private val messages = mutableListOf(
+    MessageRaw("Hello tomek! How are things\nAndroid projects suck", MessageType.IN),
+    MessageRaw("they are all a bit boring", MessageType.IN),
+    MessageRaw("Hey Johny, you quite right", MessageType.OUT),
+    MessageRaw("have you tried iOS\n that's even worse!", MessageType.OUT),
+    MessageRaw("Anyway lets go for a beer!", MessageType.IN)
+)
+
+fun isMostRecent(msgIndex: Int, messages: List<MessageRaw>) = msgIndex == messages.size - 1
+fun isSentByAnotherUser(msgIndex: Int, messages: List<MessageRaw>) = msgIndex > 0 && messages[msgIndex].type != messages[msgIndex - 1].type
+
+fun tail(msgIndex: Int, raw: MessageRaw, messages: List<MessageRaw>): Boolean {
+    return isMostRecent(msgIndex, messages) && isSentByAnotherUser(msgIndex, messages)
+}
+
+fun loadData() {
+    messages.mapIndexed { i, messageRaw ->
+        Message(
+            messageRaw.text,
+            messageRaw.type,
+            messageRaw.time,
+            tail(i, messageRaw, messages)
+        )
+    }
+}
--- app/src/test/java/tszpinda/chat/MessageTailTest.kt
+++ app/src/test/java/tszpinda/chat/MessageTailTest.kt
@@ -10,9 +10,26 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class MessageTailTest {
+
+    private fun msg(type: MessageType = MessageType.IN, time: Long = System.currentTimeMillis()) =
+        MessageRaw("hi", type, time)
+
     @Test
     fun most_recent_in_conversation() {
-        assertEquals(4, 2 + 2)
+        assertTrue(isMostRecent(0, listOf(msg())))
+        assertTrue(isMostRecent(1, listOf(msg(), msg())))
+        assertFalse(isMostRecent(0, listOf(msg(), msg())))
+    }
+
+    @Test
+    fun sent_by_another_user() {
+        val m1 = msg(MessageType.IN)
+        val m2 = msg(MessageType.OUT)
+        assertFalse(isSentByAnotherUser(0, listOf(m1)))
+        assertFalse(isSentByAnotherUser(0, listOf(m1, m1)))
+        assertFalse(isSentByAnotherUser(1, listOf(m1, m1)))
+        assertFalse(isSentByAnotherUser(0, listOf(m1, m2)))
+        assertTrue(isSentByAnotherUser(1, listOf(m1, m2)))
     }
 
     // A message has a tail when any of the following 3 criteria are met:
